d8cc3f9d2a8e855223caffc29ffe90c4e660795f
joannaarroyo <joliuac@gmail.com>
2019-03-22 10:24:23 -0700

    added some of Nick's changes from code demo

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -7,14 +7,14 @@ var Cart = function(items) {
 };
 
 Cart.prototype.addItem = function(product, quantity) {
-  // TODO: Fill in this instance method to create a new CartItem and add it to this.items
+  // DONE: Fill in this instance method to create a new CartItem and add it to this.items
   var newItem = new CartItem(product, quantity);
   this.items.push(newItem);
 };
 
 Cart.prototype.saveToLocalStorage = function() {
   // TODO: Fill in this instance method to save the contents of the cart to localStorage
-  var itemsString = JSON.stringify(cart);
+  var itemsString = JSON.stringify(this.items);
   localStorage.setItem('itemsString', itemsString);
 };
 
--- starter-code/js/catalog.js
+++ starter-code/js/catalog.js
@@ -6,13 +6,13 @@
 var cart = new Cart([]);
 // Counter variable
 var counter = 0;
-var cartContents = document.getElementById('cartContents');
+
 
 // On screen load, we call this method to put all of the busmall options
 // (the things in the Product.allProducts array) into the drop down list.
 function populateForm() {
 
-  //TODO: Add an <option> tag inside the form's select for each product
+  //DONE: Add an <option> tag inside the form's select for each product
   var selectElement = document.getElementById('items');
   for (var i in Product.allProducts) {
     var productList = document.createElement('option');
@@ -28,7 +28,7 @@ function populateForm() {
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
 function handleSubmit(event) {
 
-  // TODO: Prevent the page from reloading
+  // DONE: Prevent the page from reloading
   event.preventDefault();
   // Do all the things ...
   addSelectedItemToCart();
@@ -38,18 +38,18 @@ function handleSubmit(event) {
 
 }
 
-// TODO: Add the selected item and quantity to the cart
+// DONE: Add the selected item and quantity to the cart
 function addSelectedItemToCart() {
+  // DONE: suss out the item picked from the select list
   var product = event.target.items.value;
+  // DONE: get the quantity
   var quantity = event.target.quantity.value;
+  // DONE: using those, add one item to the Cart
   cart.addItem(product, quantity);
 
-  // TODO: suss out the item picked from the select list
-  // TODO: get the quantity
-  // TODO: using those, add one item to the Cart
 }
 
-// TODO: Update the cart count in the header nav with the number of items in the Cart
+// DONE: Update the cart count in the header nav with the number of items in the Cart
 function updateCounter() {
   counter += Number(event.target.quantity.value);
   var pageItemCounter = document.getElementById('itemCount');
@@ -60,9 +60,11 @@ function updateCounter() {
 function updateCartPreview() {
   // TODO: Get the item and quantity from the form
   // TODO: Add a new element to the cartContents div with that information
+  var cartContents = document.getElementById('cartContents');
+  itemDiv.innerHTML = '';
+  var cartList = document.createElement('ul');
   var product = event.target.items.value;
   var quantity = event.target.quantity.value;
-  var cartList = document.createElement('ul');
   var cartListItem = document.createElement('li');
   cartListItem.textContent = product + ' - ' + quantity;
   cartList.appendChild(cartListItem);
